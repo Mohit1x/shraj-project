dfce5fa664a45713adcdc3695ee54416103e49e6
mohit1x <mohitvyas746@gmail.com>
2025-06-23 21:37:32 +0530

    fixed google translator not working

--- src/components/SocialBar.tsx
+++ src/components/SocialBar.tsx
@@ -6,56 +6,26 @@ import { FiPhone } from "react-icons/fi";
 export default function SocialBar() {
   return (
     <div className="bg-gray-800 text-white py-5 w-full">
-      {/* mobile */}
-      <div className="w-full max-w-7xl mx-auto md:hidden">
-        <div className="flex flex-col items-center justify-between gap-5">
-          <h1 className="font-bold text-xl">Manufacturers & Exporter</h1>
-          <h1 className="hover:text-[#9697d1] cursor-pointer flex items-center gap-2">
-            <span>
-              <FiPhone />
-            </span>{" "}
-            +91 8956981462
-          </h1>
-          <h1 className="hover:text-[#9697d1] cursor-pointer flex items-center gap-2">
-            <span>
-              <CiMail />
-            </span>
-            sales1@shrajindustries.com
-          </h1>
-          <div className="flex items-center gap-5">
-            <span className="cursor-pointer hover:text-[#0279DE]">
-              <FaFacebookF />
-            </span>
-            <span className="cursor-pointer hover:text-black">
-              <FaXTwitter />
-            </span>
-            <span className="cursor-pointer hover:text-[]">
-              <FaInstagram />
-            </span>
-          </div>
-          <div id="google_translate_element"></div>
-        </div>
-      </div>
       {/* desktop */}
-      <div className="w-full max-w-7xl mx-auto hidden md:block">
-        <div className="flex items-center justify-between gap-5">
-          <div className="h-10 w-[1px] bg-gray-700" />
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="flex items-center flex-wrap md:flex-nowrap justify-between gap-5 px-4 md:px-0">
+          <div className="h-10 w-[1px] bg-gray-700 hidden md:block" />
           <h1 className="font-bold text-xl">Manufacturers & Exporter</h1>
-          <div className="h-10 w-[1px] bg-gray-700" />
+          <div className="h-10 w-[1px] bg-gray-700 hidden md:block" />
           <h1 className="hover:text-[#9697d1] cursor-pointer flex items-center gap-2">
             <span>
               <FiPhone />
             </span>{" "}
             +91 8956981462
           </h1>
-          <div className="h-10 w-[1px] bg-gray-700" />
+          <div className="h-10 w-[1px] bg-gray-700 hidden md:block" />
           <h1 className="hover:text-[#9697d1] cursor-pointer flex items-center gap-2">
             <span>
               <CiMail />
             </span>
             sales1@shrajindustries.com
           </h1>
-          <div className="h-10 w-[1px] bg-gray-700" />
+          <div className="h-10 w-[1px] bg-gray-700 hidden md:block" />
           <div className="flex items-center gap-5">
             <span className="cursor-pointer hover:text-[#0279DE]">
               <FaFacebookF />
@@ -67,9 +37,9 @@ export default function SocialBar() {
               <FaInstagram />
             </span>
           </div>
-          <div className="h-10 w-[1px] bg-gray-700" />
+          <div className="h-10 w-[1px] bg-gray-700 hidden md:block" />
           <div id="google_translate_element"></div>
-          <div className="h-10 w-[1px] bg-gray-700" />
+          <div className="h-10 w-[1px] bg-gray-700 hidden md:block" />
         </div>
       </div>
     </div>
